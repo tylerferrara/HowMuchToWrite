8bdbf7400055e6b93db8d6f30796c1e5ad679c0f
Tyler Ferrara <tylerferrara123@gmail.com>
2017-11-29 15:30:48 -0500

    start over

--- src/Gen.js
+++ src/Gen.js
@@ -15,7 +15,6 @@ class Gen extends Component{
   componentWillMount() {
     let words = this.props.location.state.initNum;
     if(words > 0) {
-      console.log("LETS INIT!!!!");
       const text = Ipsum({
         count: words,
         units: 'words',
@@ -26,68 +25,9 @@ class Gen extends Component{
         format: 'plain'
       });
       this.setState({words});
-      this.sentenceGen(text);
+      console.log(text);
     }
 
-  }
-
-  sentenceGen(text) {
-    let collection = text.split(" ");
-    let textList = [];
-    let WPS = null;
-    let str;
-
-    while(collection.length > 0) {
-      str = "";
-      WPS = Math.round(5 + Math.random() * 20);
-      if(collection.length < WPS) {
-        collection.forEach((word, index) => {
-          if(index === 0) {
-            str += word.charAt(0).toUpperCase() + word.slice(1);
-          } else {
-            str += (" " + word);
-          }
-        })
-         // <FIX THIS
-        collection = ""; // collection is done
-      } else {
-        collection.splice(0, WPS).forEach((word, index) => {
-          if(index === 0) {
-            str += word.charAt(0).toUpperCase() + word.slice(1);
-          } else {
-            str += (" " + word);
-          }
-        });
-      }
-      textList.push(str + ". ");
-    }
-    this.paragraphGen(textList);
-  }
-
-  paragraphGen(textList) {
-    let tempList = textList;
-    let tempP = "";
-    let blocks = [];
-    let SPP;
-    let id = 0;
-    while(tempList.length > 0) {
-      SPP = Math.round(4 + Math.random() * 8);
-      if(SPP > tempList.length) {
-        tempList.forEach(s => {
-          tempP += s;
-        });
-        tempList = [];
-      } else {
-        for(let i = 0; i < SPP; i++) {
-          tempP += tempList[i];
-          tempList = tempList.slice(1);
-        }
-      }
-      blocks.push(<p key={id}>{tempP}</p>)
-      id++;
-      tempP = "";
-    }
-    this.setState({output: blocks});
   }
 
   render() {
